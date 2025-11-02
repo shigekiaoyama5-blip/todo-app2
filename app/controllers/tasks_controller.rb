36ccc593555fb75aef5e94e5b4199d2f780cc1d0
青山重輝 <>
2025-11-03 00:30:07 +0900

    エラーメッセージを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to task_path(@task)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
     belongs_to :user, optional: true
+    validates :title, presence: true
+    validates :description, presence: true
 end
