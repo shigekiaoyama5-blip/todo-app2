630aa6cfe156087bb3bea7ef0aee52e56ad958e5
青山重輝 <>
2025-11-03 16:29:31 +0900

    validation uniqueness を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :user, optional: true
     validates :title, presence: true
+
     validates :description, presence: true
+    validates :description, uniqueness: true
 end
