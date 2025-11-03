3fdd2592dcd948d2fecb208df54568072353a600
青山重輝 <>
2025-11-03 17:35:33 +0900

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def has_written?(task)
+    tasks.exists?(id: task.id)
+  end
 end
