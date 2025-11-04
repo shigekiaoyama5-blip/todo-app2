d7893ebe69b3e06fe2a459b258039b34890fb959
青山重輝 <>
2025-11-04 20:07:56 +0900

    comment モデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_03_035457) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_04_105919) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
