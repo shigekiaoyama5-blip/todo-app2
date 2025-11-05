51aa7ea6c8d8b5ec95e7751dc3154929ab780e97
青山重輝 <>
2025-11-05 20:11:33 +0900

    create アクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,20 @@ class CommentsController < ApplicationController
         @task = Task.find(params[:task_id])
         @comment = @task.comments.build
     end
+
+    def create
+        @task = Task.find(params[:task_id])
+        @comment = @task.comments.build(comment_params)
+        if @comment.save
+            redirect_to task_path(task), notice: 'Saved'
+        else
+            flash.now[:error] = 'Failed save'
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
 end
