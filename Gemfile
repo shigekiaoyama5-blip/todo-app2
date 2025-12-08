c2b8f3e655786eb2639ba37303ddeb5d832f2783
青山重輝 <>
2025-12-08 14:23:31 +0900

    Revert "action text をインストール"
    This reverts commit d7969077f4d1dbac96b37da75ea9a95faa0d81f5.

--- Gemfile
+++ Gemfile
@@ -47,9 +47,7 @@ gem "kamal", require: false
 gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-gem "image_processing", "~> 1.2"
-gem 'webpacker', '~> 4.0', '>= 4.0.7'
-gem 'psych', '~> 3.1'
+# gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module TodoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-
-    config.active_storage.variant_processor = :mini_magick
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
